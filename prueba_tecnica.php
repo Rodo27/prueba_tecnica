ef615af48b1ac1c132ef5732837c4b597ce06c4f
Aldo Rodrigo Hernandez <aldo.hernandez@cmgo.org.mx>
2022-08-12 13:48:06 -0500

    add useful comments

--- prueba_tecnica.php
+++ prueba_tecnica.php
@@ -2,16 +2,32 @@
 
     // Prueba Técnica
 
+    /*
+
+        El la siguiente función determina si una cadena que contine los siguientes carácteres '{', '[' , '(' , ')' , ']', '}' es válida. 
+        Una cadena es válida si contine caracteres de apertura y cierre de un mismo tipo en el mismo nivel,
+        por ejemplo :
+        
+        "[]" o "{}" o "()" : Es válida y son las cadenas más simples.
+        "[{]" : No es válida.
+        "{[]" : No es válida.
+        "[{()}]": Es válida.
+    */
+
+
+    // Main function
+
     function validateString(string $myString){
 
-        $chars = [];
-        $myString =str_split($myString); 
+        $chars = [];  // Usaremos el arreglo como pila (stack)
 
-        for($i = 0; $i < count($myString); $i++){
+        $myString = str_split($myString); // convertimos la cadena a un array 
 
-            if($myString[$i] == '(' || $myString[$i] == '[' || $myString[$i] == '{')
-                array_push($chars, $myString[$i]);
-            else if ($myString[$i] == ')' && count($chars) != 0 && $chars[count($chars)-1] == '(')
+        for($i = 0; $i < count($myString); $i++){ // iteramos el array 
+
+            if($myString[$i] == '(' || $myString[$i] == '[' || $myString[$i] == '{')  // revisamos si el carácter es de apertura 
+                array_push($chars, $myString[$i]);  // agregamos a la pila
+            else if ($myString[$i] == ')' && count($chars) != 0 && $chars[count($chars)-1] == '(') // revisamos si el carácter es de cierre, que no sea la posición 0 del array y si en la pila se encuentra su carácter de apertura
                 array_pop($chars);
             else if ($myString[$i] == ']' && count($chars) != 0 && $chars[count($chars)-1] == '[')
                 array_pop($chars);
@@ -21,9 +37,11 @@
                 return false;
         }
 
-        return count($chars) == 0;
+        return count($chars) == 0; // devolvemos un boolean
         
     }
+
+    // Reutilizando la función anterior podemos validar arreglos de cadenas, simplente iterando
 
     function validateArrayOfStrings(array $arrayOfStrings){
 
@@ -45,6 +63,7 @@
     var_dump(validateArrayOfStrings($test2));
     var_dump(validateArrayOfStrings($test3));
     var_dump(validateArrayOfStrings($test4));
-    
+
+
 
 
